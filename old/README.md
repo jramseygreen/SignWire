357b88c5d141e2908e51c3746c648251cb0b6072
Josh <j.ramsey.green@gmail.com>
2021-04-04 14:55:59 +0100

    attempt 2

---
+++ old/README.md
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- saved from url=(0036)http://localhost:8888/view/test.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  
+  <title>test.html</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script data-dapp-detection="">!function(){let e=!1;function n(){if(!e){const n=document.createElement("meta");n.name="dapp-detected",document.head.appendChild(n),e=!0}}if(window.hasOwnProperty("ethereum")){if(window.__disableDappDetectionInsertion=!0,void 0===window.ethereum)return;n()}else{var t=window.ethereum;Object.defineProperty(window,"ethereum",{configurable:!0,enumerable:!1,set:function(e){window.__disableDappDetectionInsertion||n(),t=e},get:function(){if(!window.__disableDappDetectionInsertion){const e=arguments.callee;e&&e.caller&&e.caller.toString&&-1!==e.caller.toString().indexOf("getOwnPropertyNames")||n()}return t}})}}();</script></head>
+
+<body>
+  <style type="text/css">
+    html, body, #container {
+      height: 100%;
+    }
+    body, #container {
+      overflow: hidden;
+      margin: 0;
+    }
+    #iframe {
+      width: 100%;
+      height: 100%;
+      border: none;
+    }
+  </style>
+  <div id="container">
+    <iframe id="iframe" sandbox="allow-scripts" src="./test_files/test.html"></iframe>
+  </div>
+
+
+</body></html>
--- server.py
+++ server.py
@@ -1,201 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 13 13:45:39 2021
-
-@author: josh
-"""
-import threading
-import sys
 import socket
-
-import Q
-import file_io
-import file_grabber
+import threading
 
 
-def send(main, client, content):
-    client.sendall(content.encode())
+def clientThread(c, addr):
+    message = ""
+    while message != "quit":
+        message = c.recv(1024).decode()
+        print("message recieved is: ",message)
+        if message == "ping":
+            c.sendall(("pong").encode())
+
+        elif message == "file_rq":
+            send_file(c)
+        #default
+        else:
+            c.sendall(("error").encode())
+
+    print("closed client", c, addr)
+    c.close()
 
 
-def rc_file_rq(main, q, client):
-    try:
-        if q.peak() in main.meta_data:
-            path = main.meta_data[q.pop()][0]
-            data = file_io.read_bytes(path)
-            if data is not None:
-                client.sendall(data)
-            else:
-                client.sendall("fail".encode())
-    except:
-        client.sendall("fail".encode())
+def send_file(c):
+    c.sendall(("sending file").encode())
+    if (c.recv(1024).decode() == "ready"):
+        f = open("test.txt", "rb")
+        c.sendall(f.read())
+        f.close()
 
+host = '0.0.0.0'
+port = 10000
 
-def rc_server_list(main, q):
-    while q.peak() != 'msgend':
-        server = q.pop()
-        if server not in main.server_list:
-            main.server_list.append(server)
-        file_io.write_data(server_list, ''.join(server_list)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#todo
-def send_server_list(main):
-    s = socket.socket()
-    fails = []
-    for server in main.server_list:
-        try:
-            s.connect((server, main.port))
-            if "accepted" in s.recv(1024).decode():
-                s.sendall(("rc_server_list," + ''.join(main.server_list) + ",msgend").encode())
-        except:
-            fails.append(server)
-        s.close()
+s.bind((host, port))
 
-    # try one more time
-    for server in fails:
-        try:
-            s.connect((server, main.port))
-            if "accepted" in s.recv(1024).decode():
-                s.sendall(("rc_server_list," + ''.join(main.server_list) + ",msgend").encode())
-        except:
-            print("server", server, "offline")
-        s.close()
+s.listen()
+print("listening")
 
-
-# todo
-def rc_meta_data(main, q):
-    new_meta_data = {}
-
-    while q.peak() != "msgend":
-        command = q.pop()
-        if command == "data_start":
-            md5 = q.pop()
-            new_meta_data[md5] = []
-            while q.peak() != "data_end":
-                new_meta_data[md5].append(q.pop())
-
-    if new_meta_data != main.meta_data:
-        # deal with deletions
-        for item in main.metaDelete(new_meta_data):
-            file_io.remove_file(main.meta_data[item])
-
-        # make the update
-        main.meta_data = new_meta_data
-
-        # start a new file grabber
-        fg = threading.Thread(target=file_grabber.start(), args=(main,), daemon=True)
-        fg.start()
-
-
-def rc_meta_data_rq(main, q, client):
-    datastr = "rc_meta_data,"
-    try:
-        for item in main.meta_data:
-            datastr += "data_start," + item + ","
-            for path in item:
-                datastr += path + ","
-            datastr += "data_end,"
-        datastr += "msgend"
-        send(main, client, datastr)
-    except:
-        send(main, client, "failed")
-
-
-def threaded_client(main, client, address):
-    print("connected to ",address)
-    send(main, client, "accepted")
-    buffer = ""
-    while "msgend" not in buffer:
-        buffer += client.recv(1024).decode()
-
-    q = Q.MsgQueue()
-    q.push(buffer.split(','))
-
-    while q.has_items():
-        command = q.pop()
-        print(command)
-        if address in main.server_list:
-            # commands available to known addresses
-
-            # return a file's actual data
-            if "rc_file_rq" in command:
-                rc_file_rq(main, q, client)
-
-            # process new server_list
-            elif "rc_server_list" in command:
-                rc_server_list(main, q)
-
-            # process new hash table
-            elif "rc_meta_data" in command:
-                rc_meta_data(main, q)
-
-            # return the hash table
-            elif "rc_meta_data_rq" in command:
-                rc_meta_data_rq(main, client, q)
-
-            # kill
-            elif "msgend" in command:
-                client.close()
-
-            # delete an item from the server_list
-            # TODO
-            #elif "rc_server_del" in command:
-                #rc_server_del()
-
-        # accessible by all
-        # server joining a network gets their address stored and the server_list gets sent to all
-        if main.password in command:
-            if q.pop() == "msgend":  # remove msg end of join request
-                q.push(address)
-                q.push("msgend")
-                rc_server_list(main, q)  # update local server list
-                rc_meta_data_rq(main, client, q)  # send hash table
-                send_server_list(main.server_list)  # update all server lists
-            else:
-                client.close()
-                break
-
-
-def start(main):
-    print("boop")
-    host = 'localhost'  # 0.0.0.0
-    port = main.port
-
-    # join network
-    if main.server_list != []:
-        for server in main.server_list:
-            try:
-                print(main.server_list)
-                main.s.connect((server, port))
-                # successful connection
-                if 'accepted' in main.s.recv(1024).decode():
-                    # join a network
-                    main.s.sendall(main.password.encode() + ",msgend".encode())
-
-                    # receive hash table data
-                    buffer = ""
-                    while "msgend" not in buffer:
-                        buffer += main.s.recv(1024).decode()
-
-                    # push it onto a queue
-                    q = Q.MsgQueue()
-                    q.push(buffer.split(','))
-                    if q.peak() == 'rc_meta_data':
-                        q.pop()
-
-                        # process the updated hash table
-                        rc_meta_data(main, q)
-            except:
-                sys.exit("all your servers are offline, or you are offline")
-
-
-    # use main socket for listening
-    main.s.bind((host, port))
-    print('Waiting for a Connection..')
-    main.s.listen()
-
-    while True:
-        client, address = main.s.accept()
-        print('Connected to: ' + address[0] + ':' + str(address[1]))
-
-        c = threading.Thread(target=threaded_client, args=(main, client, address[0]))
-        c.start()
+while True:
+    c, addr = s.accept()
+    print("accepted", c, addr)
+    x = threading.Thread(target=clientThread, args=(c, addr,))
+    x.start()
